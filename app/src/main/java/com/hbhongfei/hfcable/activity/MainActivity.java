c93b2be2fd1a2b64c6572a23c1bf7e932fe187d5
tanyadong <2624073973@qq.com>
2016-07-19 18:04:19 +0800

    第二次更新

---
+++ app/src/main/java/com/hbhongfei/hfcable/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hbhongfei.hfcable.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hbhongfei.hfcable.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
