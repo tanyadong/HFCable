b05ef3a277a622fb14e74ef79f9ac083c30af966
YXY <1042986818@qq.com>
2016-07-19 20:24:18 +0800

    test

--- app/src/main/java/com/hbhongfei/hfcable/MainActivity.java
+++ app/src/main/java/com/hbhongfei/hfcable/MainActivity.java
@@ -10,4 +10,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * 苑雪元
+     */
+    public void test(){
+
+    }
 }
