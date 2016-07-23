eed21c4bf7154e77a0cec720030b7887c2527860
YXY <1042986818@qq.com>
2016-07-23 12:05:20 +0800

    完善昵称部分

--- app/src/main/java/com/hbhongfei/hfcable/activity/MyNameActivity.java
+++ app/src/main/java/com/hbhongfei/hfcable/activity/MyNameActivity.java
@@ -1,39 +1,72 @@
 package com.hbhongfei.hfcable.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hbhongfei.hfcable.R;
 
-public class MyNameActivity extends AppCompatActivity {
+public class MyNameActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText myName;
     private TextView number;
     private String S_name;
+    private ImageView cancel, done;
+    private Boolean tag = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_name);
+        toolBar();
         initView();
         count();
+        setOnClick();
+    }
+
+    /**
+     * Toast
+     *
+     * @param s 要显示的内容
+     */
+    private void toast(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 展示toolbar
+     */
+    private void toolBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.myName_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 
     /**
      * 初始化界面
      */
-    private void initView(){
+    private void initView() {
         myName = (EditText) findViewById(R.id.Etext_myName_text);
         number = (TextView) findViewById(R.id.Tview_myName_number);
+        cancel = (ImageView) findViewById(R.id.Image_myName_cancel);
+        done = (ImageView) findViewById(R.id.Image_myName_done);
     }
 
     /**
      * 统计用户输入的字符数，并显示
      */
-    private void count(){
+    private void count() {
         myName.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -50,8 +83,7 @@ public class MyNameActivity extends AppCompatActivity {
             public void afterTextChanged(Editable s) {
                 getValues();
                 System.out.println(S_name);
-                number.setText(""+S_name.length());
-//                System.out.println(S_name.length());
+                number.setText("" + S_name.length());
             }
         });
     }
@@ -59,7 +91,64 @@ public class MyNameActivity extends AppCompatActivity {
     /**
      * 获取数据
      */
-    private void getValues(){
+    private void getValues() {
         S_name = myName.getText().toString().trim();
+    }
+
+    /**
+     * 设置点击事件
+     */
+    private void setOnClick() {
+        this.cancel.setOnClickListener(this);
+        this.done.setOnClickListener(this);
+    }
+
+    /**
+     * 界面跳转事件
+     *
+     * @param c 要跳转的界面的class
+     */
+    private void intent(Class c) {
+        Intent i = new Intent();
+        i.setClass(MyNameActivity.this, c);
+        startActivity(i);
+    }
+
+    /**
+     * 检查数据
+     *
+     * @return 数据是否则正确
+     */
+    private boolean checkValues() {
+        //获取数据
+        getValues();
+        if (TextUtils.isEmpty(S_name)) {
+            toast("昵称不能为空，请输入");
+            tag = false;
+        }
+        return tag;
+    }
+
+    /**
+     * 将数据进行保存
+     */
+    private void saveValues() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.Image_myName_cancel:
+                intent(MyInfoActivity.class);
+                break;
+            case R.id.Image_myName_done:
+                if (checkValues()) {
+                    toast("昵称修改成功");
+                    saveValues();
+                    intent(MyInfoActivity.class);
+                }
+                break;
+        }
     }
 }
--- app/src/main/java/com/hbhongfei/hfcable/activity/MyPasswordActivity.java
+++ app/src/main/java/com/hbhongfei/hfcable/activity/MyPasswordActivity.java
@@ -17,9 +17,9 @@ import com.hbhongfei.hfcable.R;
 
 public class MyPasswordActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText pwd_before,pwd_update,pwd_sure;
-    private String S_before,S_update,S_sure;
-    private ImageView cancel,done;
+    private EditText pwd_before, pwd_update, pwd_sure;
+    private String S_before, S_update, S_sure;
+    private ImageView cancel, done;
     private Boolean tag = true;
 
     @Override
@@ -33,36 +33,38 @@ public class MyPasswordActivity extends AppCompatActivity implements View.OnClic
 
     /**
      * Toast
+     *
      * @param s 要显示的内容
      */
-    private void toast(String s){
-        Toast.makeText(this,s,Toast.LENGTH_SHORT).show();
+    private void toast(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
     }
 
     /**
      * 展示toolbar
      */
-    private void toolBar(){
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    private void toolBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.myPassword_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
+
     /**
      * 初始化界面
      */
-    private void initView(){
+    private void initView() {
         pwd_before = (EditText) findViewById(R.id.Etext_myPassword_before);
         pwd_update = (EditText) findViewById(R.id.Etext_myPassword_update);
         pwd_sure = (EditText) findViewById(R.id.Etext_myPassword_sure);
-        cancel = (ImageView) findViewById(R.id.Iamge_myPassword_cancel);
-        done = (ImageView) findViewById(R.id.Iamge_myPassword_done);
+        cancel = (ImageView) findViewById(R.id.Image_myPassword_cancel);
+        done = (ImageView) findViewById(R.id.Image_myPassword_done);
     }
 
     /**
      * 获取数据
      */
-    private void getValues(){
+    private void getValues() {
         S_before = pwd_before.getText().toString().trim();
         S_update = pwd_update.getText().toString().trim();
         S_sure = pwd_sure.getText().toString().trim();
@@ -71,7 +73,7 @@ public class MyPasswordActivity extends AppCompatActivity implements View.OnClic
     /**
      * 设置点击事件
      */
-    private void setOnclick(){
+    private void setOnclick() {
         cancel.setOnClickListener(this);
         done.setOnClickListener(this);
     }
@@ -80,28 +82,28 @@ public class MyPasswordActivity extends AppCompatActivity implements View.OnClic
      * 校验数据
      * 进行简单的校验，非空之类,新密码是相同，原密码是否正确
      */
-    private boolean checkValues(){
+    private boolean checkValues() {
         //先获取数据
         getValues();
         //校验
-        if (TextUtils.isEmpty(S_before)){
+        if (TextUtils.isEmpty(S_before)) {
             toast("原密码为空，请填写完全");
-            tag =false;
-        }else if (TextUtils.isEmpty(S_update)){
+            tag = false;
+        } else if (TextUtils.isEmpty(S_update)) {
             toast("新密码为空,请填写完全");
-            tag =false;
-        }else if (TextUtils.isEmpty(S_sure)){
+            tag = false;
+        } else if (TextUtils.isEmpty(S_sure)) {
             toast("确认密码为空,请填写完全");
-            tag =false;
-        }else if(!passwordBefore()){
+            tag = false;
+        } else if (!passwordBefore()) {
             toast("原密码不正确");
             setValuesEmpty();
-            tag =false;
-        }else if (!S_update.equals(S_sure)){
+            tag = false;
+        } else if (!S_update.equals(S_sure)) {
             toast("密码不一致，请重新填写");
             setValuesEmpty();
-            tag =false;
-        }else {
+            tag = false;
+        } else {
             tag = true;
         }
         return tag;
@@ -110,7 +112,7 @@ public class MyPasswordActivity extends AppCompatActivity implements View.OnClic
     /**
      * 将密码置空
      */
-    private void setValuesEmpty(){
+    private void setValuesEmpty() {
         pwd_before.setText("");
         pwd_update.setText("");
         pwd_sure.setText("");
@@ -120,36 +122,37 @@ public class MyPasswordActivity extends AppCompatActivity implements View.OnClic
     /**
      * 验证原密码是否相同
      */
-    private boolean passwordBefore(){
-        return  true;
+    private boolean passwordBefore() {
+        return true;
     }
 
     /**
      * 界面跳转事件
+     *
      * @param c 要跳转的界面的class
      */
-    private void intent(Class c){
+    private void intent(Class c) {
         Intent i = new Intent();
-        i.setClass(MyPasswordActivity.this,c);
+        i.setClass(MyPasswordActivity.this, c);
         startActivity(i);
     }
 
     /**
      * 将数据进行保存
      */
-    private void saveValues(){
+    private void saveValues() {
 
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.Iamge_myPassword_cancel:
+        switch (v.getId()) {
+            case R.id.Image_myPassword_cancel:
                 intent(MyInfoActivity.class);
                 this.finish();
                 break;
-            case R.id.Iamge_myPassword_done:
-                if (checkValues()){
+            case R.id.Image_myPassword_done:
+                if (checkValues()) {
                     toast("信息保存成功");
                     saveValues();
                     intent(MyInfoActivity.class);
