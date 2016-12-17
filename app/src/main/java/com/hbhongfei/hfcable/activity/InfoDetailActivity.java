df39dd3c6c80de305719e1f02d40af970bf5579f
YXY001 <1042986818@qq.com>
2016-12-17 10:50:06 +0800

    更新资讯详情页面布局

--- app/src/main/java/com/hbhongfei/hfcable/activity/InfoDetailActivity.java
+++ app/src/main/java/com/hbhongfei/hfcable/activity/InfoDetailActivity.java
@@ -1,8 +1,13 @@
 package com.hbhongfei.hfcable.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,15 +25,33 @@ public class InfoDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_detail);
-        //返回键
-        android.support.v7.app.ActionBar actionbar = getSupportActionBar();
-        actionbar.setDisplayHomeAsUpEnabled(true);
         initView();
+        toolBar();
         try {
             setValues();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    /**
+     * 展示toolbar
+     */
+    private void toolBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_infoDetail);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setElevation(0);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            toolbar.setElevation(0);
+        }
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);//设置状态栏背景为透明
+        //使用CollapsingToolbarLayout必须把title设置到CollapsingToolbarLayout上，设置到Toolbar上则不会显示
+        CollapsingToolbarLayout mCollapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.ctl_info_detail);
+        mCollapsingToolbarLayout.setTitle("资讯详情");
+        //通过CollapsingToolbarLayout修改字体颜色
+        mCollapsingToolbarLayout.setExpandedTitleColor(Color.WHITE);//设置还没收缩时状态下字体颜色
+        mCollapsingToolbarLayout.setCollapsedTitleTextColor(Color.WHITE);//设置收缩后Toolbar上字体的颜色
     }
 
     /**
